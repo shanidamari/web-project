c34be4e4db4524ca9f14c28c9369176246b57ea6
shanidamari <57593629+shanidamari@users.noreply.github.com>
2020-02-16 20:41:46 +0200

    update 2

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const http = require('http');
 const app= require('./app');
-const port = 5000||process.env.PORT;
+const host = '0.0.0.0';
+const port = process.env.PORT || 5000;
 const server = http.createServer(app);
-server.listen(port);
+server.listen(port, host, function() {
+  console.log("Server started.......");
+});
 console.log("listening to port: "+port)
